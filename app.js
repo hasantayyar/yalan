486317c83bebee3b3d28b703840cce8a72fecf0d
hasantayyar <tayyar.besik@gmail.com>
2014-03-09 23:35:29 +0200

    typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path'),
+  , path = require('path')
   , url = require(url);
 var redis = require("redis");
 
