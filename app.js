b35bf733913b3db5bf2a666934a4fc8bed679584
hasantayyar <tayyar.besik@gmail.com>
2014-03-09 23:32:41 +0200

    debug

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path')
-var redis = require("redis"),
-        client = redis.createClient(process.env.REDISCLOUD_URL);
+  , path = require('path');
+var redis = require("redis");
+
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 
 client.on("error", function (err) {
         console.log("REDIS ERROR " + err);
