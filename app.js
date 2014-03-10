746f59621440389cd2f128a2e76165e28401d8de
hasantayyar <tayyar.besik@gmail.com>
2014-03-10 10:02:23 +0200

    no user route

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , url = require('url');
